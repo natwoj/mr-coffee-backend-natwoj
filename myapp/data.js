5d275d41f90776ee1a964d7f3d724f50a6592e72
natwoj <info@nwojcik.com>
2022-04-20 20:40:39 +0200

    css develop - index.mustache

---
+++ myapp/data.js
@@ -0,0 +1,49 @@
+
+module.exports.users = [
+    {
+        'firstname': "James",
+        'lastname': "Bond",
+        'email': "james.bond@gmail.com",
+        'password': "b6b7fb4cad4bc020f76e16889a8e9065cb708d0f8c304a8a3db609b644da9536"  
+    },
+    {
+        'firstname': "Tony",
+        'lastname': "Stark",
+        'email': "starkrulz@gmail.com",
+        'password': "a836ebba36776b21dd0f5cdca497bff65c5bdfc8411cfbfe0111f27bde1c1894"  
+    },
+    {
+        'firstname': "Ali",
+        'lastname': "G",
+        'email': "nameisnotborat@gmail.com",
+        'password': "3b5fe14857124335bb8832cc602f8edcfa12db42be36b135bef5bca47e3f2b9c"  
+    }
+]
+
+module.exports.schedules = [
+   
+   {
+        'user_id': 0,
+        'day': 1,
+        'start_at': "0PM",
+        'end_at': "0PM"
+    },
+    {
+        'user_id': 1,
+        'day': 2,
+        'start_at': "1PM",
+        'end_at': "1PM"
+    },
+    {
+        'user_id': 2,
+        'day': 3,
+        'start_at': "2PM",
+        'end_at': "2PM"
+    },
+    {
+        'user_id': 3,
+        'day': 5,
+        'start_at': "3AM",
+        'end_at': "3PM"
+    }
+] 
---
+++ myapp/functions.js
@@ -0,0 +1,9 @@
+// h1 auto typing 
+const typed = new Typed(".typing",{ //specify the element
+    strings: ["Welcome to","Schedules App","for Mr.Coffee employees"], //multiple texts
+     typeSpeed: 100,
+     bacSpeed: 60,
+     loop: true
+   })
+
+   module.exports = typed;
